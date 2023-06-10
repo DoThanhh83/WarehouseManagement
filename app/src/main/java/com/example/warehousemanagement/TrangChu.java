410b5b04b02033f4bddbd26f2b8e2e2b0a8e0c07
VietPhuc135 <ttvietphuc135@gmail.com>
2023-06-10 09:31:52 +0700

    test

--- app/src/main/java/com/example/warehousemanagement/TrangChu.java
+++ app/src/main/java/com/example/warehousemanagement/TrangChu.java
@@ -3,18 +3,32 @@ package com.example.warehousemanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class TrangChu extends AppCompatActivity {
 
     Button btnDN;
     Button btnDangki;
+    ImageView imgImport;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        imgImport = findViewById(R.id.imgImport);
+        imgImport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TrangChu.this, NhapSanPham.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
