b6e69d293f7e2041893218b13ad8e4d1abcd318f
DoThanhh83 <100203762+DoThanhh83@users.noreply.github.com>
2023-06-16 21:39:35 +0700

    ok

--- app/src/main/java/com/example/warehousemanagement/TrangChu.java
+++ app/src/main/java/com/example/warehousemanagement/TrangChu.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import com.example.warehousemanagement.additem.DanhSachImport;
+import com.example.warehousemanagement.profile.ProfilePage;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
 public class TrangChu extends AppCompatActivity {
@@ -17,6 +20,7 @@ public class TrangChu extends AppCompatActivity {
     Button btnDN;
     Button btnDangki;
     ImageView imgImport, imgExport;
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,5 +44,28 @@ public class TrangChu extends AppCompatActivity {
                 startActivity(intent1);
             }
         });
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.home_apps);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+                public boolean onNavigationItemSelected( MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.home_apps:
+                            return true;
+//                    case R.id.list_apps:
+//                        startActivity(new Intent(getApplicationContext(),
+//                                StoreManager.class));
+//                        overridePendingTransition(0, 0);
+//                        return true;
+                        case R.id.info_app:
+                            startActivity(new Intent(getApplicationContext(),
+                                    ProfilePage.class));
+                            overridePendingTransition(0, 0);
+                            return true;
+                    }
+                    return false;
+    }
+}
+        );
     }
 }
