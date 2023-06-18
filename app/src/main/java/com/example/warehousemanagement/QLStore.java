c396e2c87057d63e85e71b1c4cf3709e8b34bec3
VietPhuc135 <ttvietphuc135@gmail.com>
2023-06-18 09:02:19 +0700

    test

--- app/src/main/java/com/example/warehousemanagement/QLStore.java
+++ app/src/main/java/com/example/warehousemanagement/QLStore.java
@@ -6,16 +6,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.warehousemanagement.profile.ProfilePage;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class QLStore extends AppCompatActivity {
 
+    ImageView imgStorage, imgMarket;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store_management);
+
+        imgMarket = findViewById(R.id.imgMarket);
+        imgStorage = findViewById(R.id.imgStorage);
+
+        imgMarket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(QLStore.this, QLMarket.class);
+                startActivity(intent);
+            }
+        });
+
+        imgStorage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(QLStore.this, QLStorage.class);
+                startActivity(intent1);
+            }
+        });
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.list_apps);
